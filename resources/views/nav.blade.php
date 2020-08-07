71b39eef036193cc460882b23df9f1e69f2de33a
yasu <cc_120127@icloud.com>
2020-08-08 07:49:34 +0900

    記事一覧表示機能

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -23,8 +23,7 @@
       		</a>
 
 			<div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-				<button class="dropdown-item" type="button"
-						onclick="location.href=''">
+				<button class="dropdown-item" type="button" onclick="location.href=''">
 				マイページ
 				</button>
 				<div class="dropdown-divider"></div>
