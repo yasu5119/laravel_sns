abe0c0346236348da84f815468a09861f18e908b
yasu5119 <cc_120127@icloud.com>
2020-08-30 22:44:00 +0900

    記事更新、記事削除機能

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
     <label for="">タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+    <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
     <label for=""></label>
-    <textarea name="body" rows="16" class="form-control" required placeholder="本文">{{ old('body') }}</textarea>
+    <textarea name="body" rows="16" class="form-control" required placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
