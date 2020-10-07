122f3a307084a483671aa7fec6cad79c588e545f
yasu5119 <cc_120127@icloud.com>
2020-10-07 22:20:16 +0900

    タグ機能

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike.vue'
+import ArticleTagsInput from './components/ArticleTagsInput.vue'
 
 const app = new Vue({
     el: '#app',
     components: {
         ArticleLike,
+        ArticleTagsInput,
     }
 })
--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -4,6 +4,13 @@
     <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
+    <article-tags-input
+        :initial-tags='@json($tagNames ?? [])'
+        :autocomplete-items='@json($allTagNames ?? [])'
+    >
+    </article-tags-input>
+</div>
+<div class="form-group">
     <label for=""></label>
     <textarea name="body" rows="16" class="form-control" required placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
